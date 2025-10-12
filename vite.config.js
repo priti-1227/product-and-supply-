9ab2ef1baad334d60dfc68134b60b33bb277c770
Priti rathore <2101030400316@silveroakuni.ac.in>
2025-10-12 23:49:07 +0530

    update vite.config and vercel json for backend not have https port

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // for backend not have https so this setup have to do
+    server: {
+    proxy: {
+      '/api': {
+        target: 'http://54.167.114.0',
+        changeOrigin: true,
+      },
+    }
+  }
 })
